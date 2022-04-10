34a49af6feb8ad8deba19c12d83d11d60b221c17
Ja <uzytkownik@test.pl>
2022-04-10 10:42:38 +0200

    Added Car Entinty

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.EntityManager;
+
 public class Main {
 
     public static void main(String[] args) {
-	//test 123
-        System.out.println("test");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String result = session.createNativeQuery("SELECT version();").getSingleResult().toString();
+        System.out.println(result);
+        session.close();
+        HibernateUtil.shutdown();
+
+
     }
 }
